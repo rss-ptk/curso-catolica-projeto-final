4a22f9749807d68b86d96ad6d3eaf4c9f48a3141
rodrigo santos <rodrigo.moh@gmail.com>
2022-12-22 01:02:06 -0300

    refatorado pequenos trechos

--- src/main/java/com/example/classificados/entities/Anuncio.java
+++ src/main/java/com/example/classificados/entities/Anuncio.java
@@ -2,14 +2,7 @@ package com.example.classificados.entities;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "anuncios")
